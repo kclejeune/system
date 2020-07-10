42d7bd58d9b05331b9e60113340d6bf110e4a905
Kennan LeJeune <kennan@case.edu>
2020-07-10 13:15:49 -0500

    add gnome module for real this time

---
+++ .config/nixpkgs/modules/gnome/default.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }: {
+  dconf.settings = {
+
+  };
+
+  gtk = {
+    enable = true;
+
+#     iconTheme = {
+#       package = pkgs.yaru;
+#       name = "yaru";
+#     };
+
+    theme = {
+      package = pkgs.gnome3.gnome_themes_standard;
+      name = "Adwaita-dark";
+    };
+
+  };
+}
