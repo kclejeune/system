ca1a43d5ada391606d9ed59b1ce3e19176c23a19
Kennan LeJeune <kennan@case.edu>
2020-07-09 23:50:11 -0500

    add keybase nixos module and set nix-path to correct values

---
+++ .config/nixpkgs/modules/keybase.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ kbfs keybase keybase-gui ];
+  services.keybase.enable = true;
+  services.kbfs = {
+    enable = true;
+    # FIXME /keybase needs to be owned by user
+    mountPoint = "/keybase";
+    extraFlags = [ "-label kbfs" ];
+  };
+
+  systemd.user.services = {
+    keybase.serviceConfig.Slice = "keybase.slice";
+
+    kbfs = {
+      environment = { KEYBASE_RUN_MODE = "prod"; };
+      serviceConfig.Slice = "keybase.slice";
+    };
+
+    keybase-gui = {
+      description = "Keybase GUI";
+      requires = [ "keybase.service" "kbfs.service" ];
+      after = [ "keybase.service" "kbfs.service" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.keybase-gui}/share/keybase/Keybase";
+        PrivateTmp = true;
+        Slice = "keybase.slice";
+      };
+    };
+  };
+}
---
+++ .config/nixpkgs/modules/kitty.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }: {
+  programs.kitty = {
+    enable = true;
+    font = {
+      package = pkgs.jetbrains-mono;
+      name = "JetBrains Mono";
+    };
+    settings = {
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+      font_size = (if pkgs.stdenv.isDarwin then 14 else 12);
+      strip_trailing_spaces = "smart";
+      enable_audio_bell = "no";
+      term = "xterm-256color";
+      macos_titlebar_color = "background";
+      macos_option_as_alt = "yes";
+      include = "./theme.conf";
+    };
+  };
+
+}
