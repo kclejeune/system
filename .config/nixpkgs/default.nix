40d03b6a9cc7581b2222c0333e7476480df36be6
Kennan LeJeune <kennan@case.edu>
2020-07-29 01:10:42 -0500

    add nix darwin installer to default.nix

--- .config/nixpkgs/default.nix
+++ .config/nixpkgs/default.nix
@@ -14,6 +14,33 @@ let
   overlays =
     if isDarwin then ~/.config/nixpkgs/overlays else /etc/nixos/overlays;
 
+  install = pkgs.writeShellScriptBin "install" ''
+    set -e
+    echo >&2
+    echo >&2 "Installing..."
+    echo >&2
+    ${pkgs.lib.optionalString pkgs.stdenvNoCC.isDarwin ''
+      echo "Setting up/tm nix-darwin..."
+      cd /etc
+      echo "Backing up /etc files"
+      for file in bashrc shells skhdrc zprofile zshenv zshrc nix/nix.conf; do
+          # if an /etc config file isn't a symlink, then we should move it
+          [[ ! -L $file ]] && sudo mv $file "$file.bak" && echo "backed up $file"
+      done
+
+      cd ~
+      if (! command -v darwin-rebuild); then
+          echo >&2 "Installing nix-darwin..."
+          mkdir -p ./nix-darwin && cd ./nix-darwin
+          nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+          yes | ./result/bin/darwin-installer
+          cd .. && rm -rf ./nix-darwin
+      fi
+    ''}
+
+    ${pkgs.lib.optionalString pkgs.stdenvNoCC.isDarwin darwinRebuild}
+  '';
+
   darwinRebuild = pkgs.writeShellScriptBin "rebuild" ''
     set -e
     darwin-rebuild switch --show-trace \
@@ -32,4 +59,5 @@ let
 
   rebuild = if isDarwin then darwinRebuild else nixosRebuild;
 
-in pkgs.mkShell { buildInputs = [ rebuild ]; }
+in pkgs.mkShell { buildInputs = [ rebuild install ]; }
+
