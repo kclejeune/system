c38d5e2328e826006f7f62903664421908250fc5
Kennan LeJeune <kennan@case.edu>
2020-11-08 17:47:03 -0500

    fix grub for nixOS install

--- configuration.nix
+++ configuration.nix
@@ -36,9 +36,9 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
