d315504fc140ddc0b69792a67c0b76899b0d18fa
Kennan LeJeune <kennan@case.edu>
2020-11-21 19:09:55 -0600

    nixpkgs unstable build working again

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "darwin system config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
-    unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    stable.url = "github:nixos/nixpkgs/nixos-20.09";
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,17 +14,17 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, unstable, darwin, home-manager, ... }: {
+  outputs = inputs@{ self, nixpkgs, stable, darwin, home-manager, ... }: {
     darwinConfigurations."Randall" = darwin.lib.darwinSystem {
       modules =
         [ ./darwin-configuration.nix home-manager.darwinModules.home-manager ];
-      specialArgs = { inherit inputs nixpkgs unstable; };
+      specialArgs = { inherit inputs nixpkgs stable; };
     };
     nixosConfigurations."Phil" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
         [ ./configuration.nix home-manager.nixosModules.home-manager ];
-      specialArgs = { inherit inputs nixpkgs unstable; };
+      specialArgs = { inherit inputs nixpkgs stable; };
     };
   };
 }
