f2fdefb6eb7e431983ea83c4675b63b6d0b38a6e
Kennan LeJeune <kennan@case.edu>
2020-11-28 02:50:36 -0600

    use nixFlakes build for travis test

--- default.nix
+++ default.nix
@@ -9,59 +9,42 @@ let
   else
     "/etc/nixos/configuration.nix";
 
+  darwinBuild = ''
+    ${pkgs.nixFlakes}/bin/nix build ".#darwinConfigurations.Randall.config.system.build.toplevel"
+  '';
   darwinInstall = pkgs.writeShellScriptBin "darwinInstall" ''
     set -e
-    echo >&2
     echo >&2 "Installing Nix-Darwin..."
-    echo >&2
 
-    # setup nix-darwin global store
-    sudo mkdir -p /run || sudo ln -s private/var/run /run
+    # setup /run directory for darwin system installations
+    if ! grep -q '^run\b' /etc/synthetic.conf 2>/dev/null; then
+      echo "setting up /etc/synthetic.conf..."
+      echo -e "run\tprivate/var/run" | sudo tee -a /etc/synthetic.conf >/dev/null
+      /System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util -B 2>/dev/null || true
+      /System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util -t 2>/dev/null || true
+    fi
+    if ! test -L /run; then
+        echo "setting up /run..."
+        sudo ln -sfn private/var/run /run
+    fi
 
     # back up
     cd /etc
     for file in bashrc shells skhdrc zprofile zshenv zshrc nix/nix.conf; do
         # if an /etc config file isn't a symlink, then we should move it
-        [[ -e $file ]] && [[ ! -L $file ]] && sudo mv $file "$file.bak" && echo "backed up $file"
+        [[ -e $file ]] && [[ ! -L $file ]] && sudo mv $file "$file.backup" && echo "backed up $file"
     done
 
+    ${darwinBuild} $$ ./result/activate
+  '';
 
-    export NIX_PATH=darwin-config=${configuration}:darwin=${sources.nix-darwin}
-
-    # build nix darwin
-    nix-build ${sources.nix-darwin} -A system --no-out-link build
-
-    # build the actual configuration once we do that
-    sudo ./result/sw/bin/darwin-rebuild switch
+  darwinTest = pkgs.writeShellScriptBin "darwinTest" ''
+    ${darwinBuild}
   '';
 
   homebrewInstall = pkgs.writeShellScriptBin "homebrewInstall" ''
     ${pkgs.bash}/bin/bash -c "$(${pkgs.curl}/bin/curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   '';
 
-  darwinRebuild = pkgs.writeShellScriptBin "rebuild" ''
-    set -e
-    darwin-rebuild switch -I darwin-config=${configuration} \
-      -I nixpkgs=${sources.nixpkgs} \
-      -I darwin=${sources.nix-darwin} \
-      -I home-manager=${sources.home-manager} \
-  '';
-
-  nixosRebuild = pkgs.writeShellScriptBin "rebuild" ''
-    set -e
-    sudo nixos-rebuild switch -I nixos-config=${configuration} \
-      -I nixpkgs=${sources.nixpkgs} \
-      -I home-manager=${sources.home-manager} \
-  '';
-
-  rebuild = if isDarwin then darwinRebuild else nixosRebuild;
-
-in pkgs.mkShell {
-  buildInputs = [
-    # keep this line if you use bash
-    pkgs.bashInteractive
-    rebuild
-    darwin-bootstrap
-  ];
-}
+in pkgs.mkShell { buildInputs = [ darwinTest darwinInstall homebrewInstall ]; }
 
