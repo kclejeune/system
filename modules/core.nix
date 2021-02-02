0408db42f68e16d521f6cca7762b38c181e7b3ab
Kennan LeJeune <kennan@case.edu>
2021-02-01 22:33:54 -0500

    add use_flake wrapper, remove use flake function since it's already standard

--- modules/core.nix
+++ modules/core.nix
@@ -24,13 +24,6 @@
     direnv = {
       enable = true;
       enableNixDirenvIntegration = true;
-      stdlib = ''
-        use_flake() {
-          watch_file flake.nix
-          watch_file flake.lock
-          eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
-        }
-      '';
     };
     fzf = {
       enable = true;
