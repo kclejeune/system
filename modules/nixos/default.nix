757e9db914e14925705b9e8a1214ea67e6ca9f3a
Kennan LeJeune <kennan@case.edu>
2021-02-27 01:53:13 -0500

    use caps2esc minimal mode

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -5,12 +5,15 @@
   services.interception-tools = {
     enable = true;
     plugins = with pkgs.interception-tools-plugins; [ caps2esc ];
+    udevmonConfig = ''
+      - JOB: intercept -g $DEVNODE | caps2esc -m 1 | uinput -d $DEVNODE
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
   };
 
-  environment.systemPackages = with pkgs; [
-    vscode
-    firefox
-  ];
+  environment.systemPackages = with pkgs; [ vscode firefox ];
 
   hm = { pkgs, ... }: { imports = [ ../gnome ]; };
 }
