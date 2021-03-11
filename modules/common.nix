142854eebcbec037a60f1ec477d17e5a782e55c1
Kennan LeJeune <kennan@case.edu>
2021-03-11 02:04:17 -0500

    extract nix settings into nixpkgs module

--- modules/common.nix
+++ modules/common.nix
@@ -1,5 +1,5 @@
 { inputs, config, lib, pkgs, ... }: {
-  imports = [ ../modules/primary.nix ];
+  imports = [ ../modules/primary.nix ../modules/nixpkgs.nix ];
 
   programs.zsh.enable = true;
 
@@ -50,42 +50,6 @@
     };
     # list of acceptable shells in /etc/shells
     shells = with pkgs; [ bash zsh fish ];
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      keep-outputs = true
-      keep-derivations = true
-      ${lib.optionalString (config.nix.package == pkgs.nixFlakes)
-      "experimental-features = nix-command flakes"}
-    '';
-    trustedUsers = [ "${config.user.name}" "root" "@admin" "@wheel" ];
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 30d";
-    };
-    buildCores = 8;
-    maxJobs = 8;
-    readOnlyStore = true;
-    nixPath = [
-      "nixpkgs=/etc/${config.environment.etc.nixpkgs.target}"
-      "home-manager=/etc/${config.environment.etc.home-manager.target}"
-    ];
-
-    binaryCaches = [ "https://kclejeune.cachix.org" ];
-    binaryCachePublicKeys =
-      [ "kclejeune.cachix.org-1:fOCrECygdFZKbMxHClhiTS6oowOkJ/I/dh9q9b1I4ko=" ];
-
-    registry.nixpkgs = {
-      from = {
-        id = "nixpkgs";
-        type = "indirect";
-      };
-      flake = inputs.nixpkgs;
-    };
   };
 
   fonts = {
---
+++ modules/nixpkgs.nix
@@ -0,0 +1,43 @@
+{ inputs, config, lib, pkgs, ... }: {
+  nixpkgs = {
+    config = import ./config.nix;
+    overlays = [ ];
+  };
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      keep-outputs = true
+      keep-derivations = true
+      ${lib.optionalString (config.nix.package == pkgs.nixFlakes)
+      "experimental-features = nix-command flakes"}
+    '';
+    trustedUsers = [ "${config.user.name}" "root" "@admin" "@wheel" ];
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
+    buildCores = 8;
+    maxJobs = 8;
+    readOnlyStore = true;
+    nixPath = [
+      "nixpkgs=/etc/${config.environment.etc.nixpkgs.target}"
+      "home-manager=/etc/${config.environment.etc.home-manager.target}"
+    ];
+
+    binaryCaches =
+      [ "https://kclejeune.cachix.org" "https://nix-community.cachix.org/" ];
+    binaryCachePublicKeys = [
+      "kclejeune.cachix.org-1:fOCrECygdFZKbMxHClhiTS6oowOkJ/I/dh9q9b1I4ko="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
+    registry.nixpkgs = {
+      from = {
+        id = "nixpkgs";
+        type = "indirect";
+      };
+      flake = inputs.nixpkgs;
+    };
+  };
+}
