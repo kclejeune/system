27d154cfa27dbf299d36f63b8e87de07ae078875
Kennan LeJeune <kennan@case.edu>
2021-03-23 12:36:02 -0400

    fix dconf settings for gnome

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -1,21 +1,24 @@
 { config, pkgs, ... }: {
-  dconf.settings = {
-
-  };
+  imports = [ ./dconf.nix ];
 
   gtk = {
     enable = true;
 
     iconTheme = {
       package = pkgs.yaru-theme;
-      name = "yaru";
+      name = "Yaru";
     };
 
     theme = {
       package = pkgs.yaru-theme;
-      name = "yaru-dark";
+      name = "Yaru-dark";
     };
 
+    gtk3.extraConfig = {
+      gtk-icon-theme-name = "Yaru";
+      gtk-theme-name = "Yaru-dark";
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
   services.gnome-keyring.enable = true;
 }
