44ac064226801813570ac046ac005f2afcc21a90
Kennan LeJeune <kennan@case.edu>
2021-04-02 03:47:56 -0400

    add new aliases

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,13 +3,15 @@
   programs.git = {
     userName = "Kennan LeJeune";
     extraConfig = {
+      credential.helper = if pkgs.stdenvNoCC.isDarwin then "osxkeychain" else "cache --timeout=1000000000";
       http.sslVerify = true;
       pull.rebase = false;
     };
     aliases = {
+      commit = "commit --verbose";
+      fetch = "fetch --verbose";
       fix = "commit --amend --no-edit";
       oops = "reset HEAD~1";
-      commit = "commit --verbose";
       pull = "pull --verbose";
       push = "push --verbose";
     };
