492acb30d4ae57aafe1fe2085f653847213c2633
Kennan LeJeune <kennan@case.edu>
2021-04-02 01:02:54 -0400

    remove some unused zsh plugins

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 let
   functions = builtins.readFile ./functions.sh;
   aliases = {
@@ -32,27 +32,32 @@ in {
       ${functions}
       unset RPS1
     '';
-    plugins = [{
-      name = "zsh-syntax-highlighting";
-      src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-syntax-highlighting";
-        rev = "0.7.1";
-        sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
-      };
-    }];
+    plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.7.1";
+          sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
+        };
+      }
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "v1.0.2";
+          sha256 = "sha256-Ptxik1r6anlP7QTqsN1S2Tli5lyRibkgGlVlwWZRG3k=";
+        };
+      }
+    ];
     oh-my-zsh = {
       enable = true;
-      theme = "agnoster";
-      plugins = [
-        "z"
-        "git"
-        "sudo"
-        "command-not-found"
-        "common-aliases"
-        "history-substring-search"
-        "virtualenv"
-      ];
+      # theme = "agnoster";
+      plugins = [ "git" "sudo" "common-aliases" "z" ];
     };
   };
+
+  programs.starship = { enable = true; };
 }
