5174ebb20fdc3703e948f0c214f1a46f222c1575
Kennan LeJeune <kennan@case.edu>
2021-04-14 04:13:43 -0400

    yabai: re-enable yabai and skhd services via nix-darwin

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,3 +1,9 @@
 { pkgs, ... }: {
-  imports = [ ../common.nix ./core.nix ./brew.nix ./preferences.nix ];
+  imports = [
+    ../common.nix
+    ./core.nix
+    ./brew.nix
+    ./preferences.nix
+    ./display-manager.nix
+  ];
 }
