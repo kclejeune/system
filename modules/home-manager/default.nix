d01834cf6246fdc7aeb516af7ea12402e2385f81
Kennan LeJeune <kennan@case.edu>
2021-06-12 00:33:36 -0400

    add jdk to user env

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -20,65 +20,71 @@ in
     path = "${config.home.homeDirectory}/.nixpkgs/modules/home-manager";
   };
 
-  home = {
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "20.09";
-    sessionVariables = {
-      GPG_TTY = "/dev/ttys000";
-      EDITOR = "nvim";
-      VISUAL = "nvim";
-      CLICOLOR = 1;
-      LSCOLORS = "ExFxBxDxCxegedabagacad";
-      KAGGLE_CONFIG_DIR = "${config.xdg.configHome}/kaggle";
-    };
+  home =
+    let
+      java = pkgs.adoptopenjdk-bin;
+    in
+    {
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      stateVersion = "20.09";
+      sessionVariables = {
+        GPG_TTY = "/dev/ttys000";
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+        CLICOLOR = 1;
+        LSCOLORS = "ExFxBxDxCxegedabagacad";
+        KAGGLE_CONFIG_DIR = "${config.xdg.configHome}/kaggle";
+        JAVA_HOME = "${java}";
+      };
 
-    # define package definitions for current user environment
-    packages = with pkgs; [
-      # python with default packages
-      (python3.withPackages (ps: with ps; [ numpy scipy networkx ]))
-      python3Packages.black
-      cachix
-      coreutils-full
-      curl
-      # dust
-      fd
-      gawk
-      ghc
-      git
-      gnugrep
-      gnupg
-      gnused
-      htop
-      httpie
-      curlie
-      hyperfine
-      jq
-      kotlin
-      neofetch
-      niv
-      nixfmt
-      nixpkgs-fmt
-      nodejs
-      openssh
-      pandoc
-      pre-commit
-      ranger
-      ripgrep
-      ripgrep-all
-      rsync
-      speedtest-cli
-      sysdo
-      tectonic
-      tealdeer
-      # texlive.combined.scheme-full
-      youtube-dl
-    ];
-  };
+      # define package definitions for current user environment
+      packages = with pkgs; [
+        # python with default packages
+        (python3.withPackages (ps: with ps; [ numpy scipy networkx ]))
+        python3Packages.black
+        cachix
+        coreutils-full
+        curl
+        # dust
+        fd
+        gawk
+        ghc
+        git
+        gnugrep
+        gnupg
+        gnused
+        htop
+        httpie
+        curlie
+        hyperfine
+        java
+        jq
+        kotlin
+        neofetch
+        niv
+        nixfmt
+        nixpkgs-fmt
+        nodejs
+        openssh
+        pandoc
+        pre-commit
+        ranger
+        ripgrep
+        ripgrep-all
+        rsync
+        speedtest-cli
+        sysdo
+        tectonic
+        tealdeer
+        # texlive.combined.scheme-full
+        youtube-dl
+      ];
+    };
 }
