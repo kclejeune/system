596451aac7194c39e3f6045f96f756f06d8051fa
Kennan LeJeune <kennan@case.edu>
2021-06-17 00:16:28 -0400

    bump macos-11 for github actions, update nix flake version

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
 
-# URL="https://nixos.org/nix/install"
-URL="https://github.com/numtide/nix-flakes-installer/releases/download/nix-2.4pre20210126_f15f0b8/install"
+RELEASE="nix-2.4pre20210604_8e6ee1b"
+URL="https://github.com/numtide/nix-unstable-installer/releases/download/$RELEASE/install"
 
 # install using workaround for darwin systems
 [[ $(uname -s) = "Darwin" ]] && FLAG="--darwin-use-unencrypted-nix-store-volume"
 [[ ! -z "$1" ]] && URL="$1"
 
-command -v nix > /dev/null && echo "nix is already installed on this system." || bash <(curl -L $URL) --daemon $FLAG
+if command -v nix > /dev/null; then
+    echo "nix is already installed on this system."
+else
+    bash <(curl -L $URL) --daemon $FLAG
+fi
