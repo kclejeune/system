8b7792e1e92dd7ce52b4c8a414a54308ac5ed350
Kennan LeJeune <kennan@case.edu>
2021-07-30 00:22:17 -0400

    nvim: just kidding back to COC

--- modules/home-manager/vim/plugins/default.nix
+++ modules/home-manager/vim/plugins/default.nix
@@ -1,26 +1,26 @@
 { lib, ... }: {
   imports = [
-    ./bash
-    ./css
-    ./go-lsp
-    ./html
-    ./json
-    ./latex
-    ./lspsaga-nvim
+    # ./bash
+    # ./css
+    # ./go-lsp
+    # ./html
+    # ./json
+    # ./latex
+    # ./lspsaga-nvim
     # ./lua-lsp # build failure
-    ./lualine-nvim
-    ./nvim-autopairs
-    ./nvim-compe
-    ./nvim-lspconfig
-    ./pyright-lsp
-    ./rnix-lsp
-    ./svelte-lsp
-    ./typescript-lsp
-    ./vimscript-lsp
-    ./vsnip
-    ./vue-lsp
-    ./yaml-lsp
-    # ./coc
+    # ./lualine-nvim
+    # ./nvim-autopairs
+    # ./nvim-compe
+    # ./nvim-lspconfig
+    # ./pyright-lsp
+    # ./rnix-lsp
+    # ./svelte-lsp
+    # ./typescript-lsp
+    # ./vimscript-lsp
+    # ./vsnip
+    # ./vue-lsp
+    # ./yaml-lsp
+    ./coc
 
     ./theme
     ./treesitter
