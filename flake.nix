f2ba81797cb6cf53aafa0bf3c1f5c5c5f84d0ff1
Kennan LeJeune <kennan@case.edu>
2021-09-24 05:56:37 -0400

    devshell: update $DEVSHELL_ROOT to $PRJ_ROOT

--- flake.nix
+++ flake.nix
@@ -210,7 +210,7 @@
         ${pkgs.nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
       '';
       sysdo = pkgs.writeShellScriptBin "sysdo" ''
-        cd $DEVSHELL_ROOT && ${pyEnv}/bin/python3 bin/do.py $@
+        cd $PRJ_ROOT && ${pyEnv}/bin/python3 bin/do.py $@
       '';
       fmt = pkgs.writeShellScriptBin "fmt" ''
         ${pkgs.nixfmt}/bin/nixfmt $@ && ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt $@
