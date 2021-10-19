9d1db2bb596825f4d4bbfe685607be52a33b7774
Kennan LeJeune <kennan@case.edu>
2021-10-19 02:07:30 -0400

    sysdo: add shell completions

--- modules/home-manager/cli/functions.sh
+++ modules/home-manager/cli/functions.sh
@@ -55,3 +55,14 @@ function service() {
         launchctl $1 $service
     fi
 }
+
+_dopy_completion() {
+    local IFS=$'
+'
+    COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   _DO.PY_COMPLETE=complete_bash $1 ) )
+    return 0
+}
+
+complete -o default -F _dopy_completion sysdo
