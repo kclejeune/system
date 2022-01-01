276e977a3817b50848b55ca6dbbee354f9067bd0
Kennan LeJeune <kennan@case.edu>
2022-01-01 15:31:57 -0500

    separate installer checks

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -7,9 +7,9 @@ if [[ $(uname -s) = "Darwin" ]]; then
     FLAG="--darwin-use-unencrypted-nix-store-volume"
 fi
 
-[[ ! -z "$1" ]] && URL="$1"
+[[ -n "$1" ]] && URL="$1"
 
-if command -v nix > /dev/null; then
+if command -v nix >/dev/null; then
     echo "nix is already installed on this system."
 else
     bash <(curl -L $URL) --daemon $FLAG
@@ -17,9 +17,9 @@ fi
 
 NIX_CONF_PATH=$HOME/.config/nix
 if [[ ! -d $NIX_CONF_PATH ]]; then
-    mkdir -p $NIX_CONF_PATH
+    mkdir -p "$NIX_CONF_PATH"
 fi
 
-if [[ ! -f $NIX_CONF_PATH/nix.conf ]] || ! grep "experimental-features" < $NIX_CONF_PATH; then
-    echo "experimental-features = nix-command flakes" | tee -a $NIX_CONF_PATH/nix.conf
+if [[ ! -f $NIX_CONF_PATH/nix.conf ]] || ! grep "experimental-features" <"$NIX_CONF_PATH"; then
+    echo "experimental-features = nix-command flakes" | tee -a "$NIX_CONF_PATH/nix.conf"
 fi
