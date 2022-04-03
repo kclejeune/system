448beaff2d71b10954b1c28604021524265e3aee
Kennan LeJeune <kennan@case.edu>
2022-04-03 01:09:37 -0400

    re-enable homebrew auto updating

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -42,7 +42,7 @@ in
         KAGGLE_CONFIG_DIR = "${config.xdg.configHome}/kaggle";
         JAVA_HOME = "${pkgs.openjdk.home}";
         NODE_PATH = "${NODE_GLOBAL}/lib";
-        HOMEBREW_NO_AUTO_UPDATE = 1;
+        # HOMEBREW_NO_AUTO_UPDATE = 1;
       };
       sessionPath = [ "${NODE_GLOBAL}/bin" ];
 
