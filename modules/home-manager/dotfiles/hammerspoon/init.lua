59e23ab56cb23fa337c6d4987624d28f83dc3ac9
Kennan LeJeune <kennan@case.edu>
2022-04-03 15:51:14 -0400

    clean up hotkey config for ReloadConfiguration

--- modules/home-manager/dotfiles/hammerspoon/init.lua
+++ modules/home-manager/dotfiles/hammerspoon/init.lua
@@ -1,5 +1,3 @@
-require("yabai")
-
 -- install hammerspoon cli
 local brewPrefixOutput, _, _, _ = hs.execute("brew --prefix", true)
 local brewPrefix = string.gsub(brewPrefixOutput, "%s+", '')
@@ -7,8 +5,11 @@ require("hs.ipc")
 local ipc = hs.ipc.cliInstall(brewPrefix)
 print(string.format("ipc: %s", ipc))
 
+-- import keybindings for yabai
+require("yabai")
+
 -- Make all our animations really fast
-hs.window.animationDuration = 0.1
+hs.window.animationDuration = 0
 
 -- Load SpoonInstall, so we can easily load our other Spoons
 hs.loadSpoon("SpoonInstall")
@@ -23,8 +24,8 @@ Install:andUse("RoundedCorners", {
     }
 })
 Install:andUse("ReloadConfiguration", {
-    start = true
-})
-spoon.ReloadConfiguration:bindHotkeys({
-    reloadConfiguration = {{"cmd", "ctrl", "shift"}, "r"}
+    start = true,
+    hotKeys = {
+        reloadConfiguration = {{"cmd", "ctrl", "shift"}, "r"}
+    }
 })
