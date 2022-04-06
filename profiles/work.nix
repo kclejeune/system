24aada3f595c35d0f03b0e132278c6124b740cb4
Kennan LeJeune <kennan@case.edu>
2022-04-06 14:34:24 -0400

    fix work proxy

--- profiles/work.nix
+++ profiles/work.nix
@@ -13,6 +13,6 @@
             (lib.attrsets.filterAttrs (isValidCertFile validExtensions)
               (builtins.readDir path))));
     in
-    [ "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ]
-    ++ (builtins.concatMap (getCertFiles [ "cer" "crt" "pem" ]) [ "/etc/certs.d" ]);
+    [ "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" "/etc/ssl/certs/apl.pem" ]
+    ++ (builtins.concatMap (getCertFiles [ "cer" "crt" "pem" ]) [ "/etc/ssl/certs" ]);
 }
