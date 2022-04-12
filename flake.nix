6014a0241175bfb7edee46454f261f41ea8c30dd
Kennan LeJeune <kennan@case.edu>
2022-04-12 00:36:33 -0400

    revert to prior home-manager to fix broken activation

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
