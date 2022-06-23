942ce8a850e9f5ecb5c5f16a7c320055553990be
Kennan LeJeune <kennan@case.edu>
2022-06-22 22:58:32 -0400

    wrap plugin config to exclude if running in vscode

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -8,44 +8,49 @@
       ${luaConfig}
       EOF
     '';
-    readVimConfig = file:
+    readVimConfigRaw = file:
       if (lib.strings.hasSuffix ".lua" (builtins.toString file)) then
-        wrapLuaConfig (builtins.readFile file) else
+        wrapLuaConfig (builtins.readFile file)
+      else
         builtins.readFile file;
+    readVimConfig = file: ''
+      if !exists('g:vscode')
+        ${readVimConfigRaw file}
+      endif
+    '';
     pluginWithCfg = { plugin, file }: {
       inherit plugin;
       config = readVimConfig file;
     };
   };
 
-  programs.neovim =
-    {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
 
-      # nvim plugin providers
-      withNodeJs = false;
-      withRuby = false;
-      withPython3 = false;
+    # nvim plugin providers
+    withNodeJs = false;
+    withRuby = false;
+    withPython3 = false;
 
-      # share vim plugins since nothing is specific to nvim
-      plugins = with pkgs.vimPlugins; [
-        # basics
-        vim-sensible
-        vim-fugitive
-        vim-sandwich
-        vim-commentary
-        vim-nix
+    # share vim plugins since nothing is specific to nvim
+    plugins = with pkgs.vimPlugins; [
+      # basics
+      vim-sensible
+      vim-fugitive
+      vim-sandwich
+      vim-commentary
+      vim-nix
 
-        # vim addon utilities
-        direnv-vim
-        ranger-vim
-      ];
-      extraConfig = ''
-        ${config.lib.vimUtils.readVimConfig ./settings.lua}
-      '';
-    };
+      # vim addon utilities
+      direnv-vim
+      ranger-vim
+    ];
+    extraConfig = ''
+      ${config.lib.vimUtils.readVimConfigRaw ./settings.lua}
+    '';
+  };
 
 }
--- modules/home-manager/nvim/plugins/coc/default.nix
+++ modules/home-manager/nvim/plugins/coc/default.nix
@@ -2,43 +2,41 @@
   # link coc-settings to the right location
   xdg.configFile."nvim/coc-settings.json".source = ./coc-settings.json;
 
-  programs.neovim =
-
-    {
-      extraPackages = with pkgs; [
-        rubyPackages.solargraph
-        nodePackages.pyright
-        rnix-lsp
-        fzf
-      ];
-      plugins = with pkgs.vimPlugins; [
-        (config.lib.vimUtils.pluginWithCfg {
-          plugin = coc-nvim;
-          file = ./coc-nvim.vim;
-        })
-        coc-css
-        coc-eslint
-        coc-fzf
-        coc-git
-        coc-go
-        coc-html
-        coc-java
-        coc-json
-        coc-lua
-        coc-pairs
-        coc-prettier
-        coc-pyright
-        coc-r-lsp
-        coc-rls
-        coc-smartf
-        coc-snippets
-        coc-solargraph
-        coc-tslint
-        coc-tsserver
-        coc-vetur
-        coc-vimlsp
-        coc-vimtex
-        coc-yaml
-      ];
-    };
+  programs.neovim = {
+    extraPackages = with pkgs; [
+      rubyPackages.solargraph
+      nodePackages.pyright
+      rnix-lsp
+      fzf
+    ];
+    plugins = with pkgs.vimPlugins; [
+      (config.lib.vimUtils.pluginWithCfg {
+        plugin = coc-nvim;
+        file = ./coc-nvim.vim;
+      })
+      coc-css
+      coc-eslint
+      coc-fzf
+      coc-git
+      coc-go
+      coc-html
+      coc-java
+      coc-json
+      coc-lua
+      coc-pairs
+      coc-prettier
+      coc-pyright
+      coc-r-lsp
+      coc-rls
+      coc-smartf
+      coc-snippets
+      coc-solargraph
+      coc-tslint
+      coc-tsserver
+      coc-vetur
+      coc-vimlsp
+      coc-vimtex
+      coc-yaml
+    ];
+  };
 }
