a7f6f6ee17b63281cd5ca42242bcb8ae9517c858
Kennan LeJeune <kennan@case.edu>
2022-07-20 22:48:36 -0400

    allow broken pyOpenSSL

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -3,7 +3,7 @@ let theme = builtins.readFile ./theme.conf;
 in
 {
   programs.kitty = {
-    enable = false;
+    enable = true;
     package = pkgs.kitty;
     font = {
       package = pkgs.jetbrains-mono;
--- modules/overlays.nix
+++ modules/overlays.nix
@@ -6,5 +6,28 @@
       stable = import inputs.stable { system = prev.system; };
       small = import inputs.small { system = prev.system; };
     })
+    (final: prev: {
+      python3 = prev.python3.override {
+        packageOverrides = (pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = old.meta // { broken = false; };
+          });
+        });
+      };
+      python39 = prev.python39.override {
+        packageOverrides = (pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = old.meta // { broken = false; };
+          });
+        });
+      };
+      python310 = prev.python310.override {
+        packageOverrides = (pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = old.meta // { broken = false; };
+          });
+        });
+      };
+    })
   ];
 }
