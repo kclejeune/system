e6fc6d2acf7f6f8aed999e9385de6dee0237e47a
Kennan LeJeune <kennan@case.edu>
2023-04-12 14:43:01 -0400

    add additional certs

--- profiles/work.nix
+++ profiles/work.nix
@@ -2,5 +2,5 @@
   user.name = "lejeukc1";
   hm = {imports = [./home-manager/work.nix];};
 
-  security.pki.certificateFiles = ["${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" "/etc/certs.d/apl.pem"];
+  security.pki.certificateFiles = ["${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" "/etc/certs.d/apl.pem" "/etc/certs.d/dod-chain.pem"];
 }
