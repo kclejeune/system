0e3655e14e086a336faec2bd5618ce393fe9b5a6
Kennan LeJeune <kennan@case.edu>
2023-04-29 10:37:54 -0400

    use floating devenv tag

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     # package repos
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    devenv.url = "github:cachix/devenv/v0.6.2";
+    devenv.url = "github:cachix/devenv/latest";
 
     # system management
     nixos-hardware.url = "github:nixos/nixos-hardware";
