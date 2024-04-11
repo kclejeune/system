682688891bd234fb911baefad4d6f5b9fcb1386c
Kennan LeJeune <kennan@case.edu>
2024-04-11 10:06:50 -0400

    use USER env variable before `id` if available

--- bin/do.py
+++ bin/do.py
@@ -46,7 +46,9 @@ else:
     # in all other cases of linux
     PLATFORM = FlakeOutputs.HOME_MANAGER
 
-USERNAME = subprocess.run(["id", "-un"], capture_output=True).stdout.decode().strip()
+USERNAME = os.getenv(
+    "USER", subprocess.run(["id", "-un"], capture_output=True).stdout.decode().strip()
+)
 SYSTEM_ARCH = "aarch64" if UNAME.machine == "arm64" else UNAME.machine
 SYSTEM_OS = UNAME.system.lower()
 DEFAULT_HOST = f"{USERNAME}@{SYSTEM_ARCH}-{SYSTEM_OS}"
