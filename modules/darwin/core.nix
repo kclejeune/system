db0b44f0724075a1b2eac84bdd82e8bbcefb1611
Kennan LeJeune <kennan@case.edu>
2024-12-04 21:28:28 -0500

    fix nix path

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -5,13 +5,17 @@
 }: {
   # environment setup
   environment = {
-    etc = {darwin.source = "${inputs.darwin}";};
+    etc = {
+      darwin.source = "${inputs.darwin}";
+      nixpkgs.source = "${inputs.nixpkgs}";
+      stable.source = "${inputs.stable}";
+    };
   };
 
   # auto manage nixbld users with nix darwin
   nix = {
     configureBuildUsers = false;
-    nixPath = ["darwin=/etc/${config.environment.etc.darwin.target}"];
+    nixPath = ["darwin=/etc/${config.environment.etc.darwin.target}" "nixpkgs=/etc/${config.environment.etc.nixpkgs.target}" "stable=/etc/${config.environment.etc.stable.target}"];
     extraOptions = ''
       extra-platforms = x86_64-darwin aarch64-darwin
     '';
