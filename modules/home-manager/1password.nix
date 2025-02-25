fcc12f2098b3bd30d807199f4e155fff3b5c4657
Kennan LeJeune <kc.lejeune@gmail.com>
2025-02-25 12:13:21 -0500

    fix deprecated gpg signer option

--- modules/home-manager/1password.nix
+++ modules/home-manager/1password.nix
@@ -34,7 +34,7 @@ in {
     signing = {
       signByDefault = true;
       key = null;
-      gpgPath =
+      signer =
         if pkgs.stdenvNoCC.isDarwin
         then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
         else "${pkgs._1password-gui}/share/1password/op-ssh-sign";
