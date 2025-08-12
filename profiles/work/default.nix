ef5a1cbab6862bcabe1f53eed5d068e0d9a3829f
Kennan LeJeune <kc.lejeune@gmail.com>
2025-08-12 11:04:16 -0400

    overlay unstable nix_2_18 to nixpkgs-25.05

--- profiles/work/default.nix
+++ profiles/work/default.nix
@@ -1,7 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   user.name = "klejeune";
   hm = {
     imports = [./home-manager];
   };
   security.pki.installCACerts = false;
+  nix.package = pkgs.nixVersions.nix_2_18;
 }
