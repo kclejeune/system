047fa02b07f1d6d84af927c63217c54cf9607403
Kennan LeJeune <kc.lejeune@gmail.com>
2026-01-05 14:30:19 -0500

    disable codespell

--- modules/home-manager/nvim/lua/plugins/default.lua
+++ modules/home-manager/nvim/lua/plugins/default.lua
@@ -86,10 +86,7 @@ return {
                     end
                 end,
                 -- Use the "*" filetype to run formatters on all filetypes.
-                ["*"] = { "codespell" },
-                -- Use the "_" filetype to run formatters on filetypes that don't
-                -- have other formatters configured.
-                ["_"] = { "trim_whitespace" },
+                ["*"] = { "trim_whitespace" },
             },
             -- Set this to change the default values when calling conform.format()
             -- This will also affect the default values for format_on_save/format_after_save
