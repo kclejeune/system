dacde99de0a59ea427655f46e90f311c83907fc2
Kennan LeJeune <kennan@case.edu>
2026-03-22 20:34:43 -0400

    add authelia config

--- modules/nixos/gateway.nix
+++ modules/nixos/gateway.nix
@@ -2,6 +2,12 @@
   config,
   ...
 }:
+let
+  autheliaInstance = "main";
+  autheliaUser = "authelia-${autheliaInstance}";
+  domain = "kclj.io";
+  autheliaPort = 9091;
+in
 {
   networking.hostName = "gateway";
   networking.domain = "";
@@ -33,12 +39,89 @@
     };
   };
 
-  # TODO: Authelia - authentication server (disabled until secrets/domains are configured)
-  # services.authelia.instances.main = {
-  #   enable = true;
-  #   settings = { ... };
-  #   secrets = { ... };
-  # };
+  # sops-nix - decrypts using the SSH host key via ssh-to-age
+  sops = {
+    defaultSopsFile = ../../secrets/gateway.yaml;
+
+    secrets = {
+      "authelia/jwt_secret" = {
+        owner = autheliaUser;
+      };
+      "authelia/session_secret" = {
+        owner = autheliaUser;
+      };
+      "authelia/storage_encryption_key" = {
+        owner = autheliaUser;
+      };
+      "authelia/users" = {
+        owner = autheliaUser;
+        path = "/var/lib/authelia-${autheliaInstance}/users.yaml";
+      };
+    };
+  };
+
+  # Authelia - authentication server
+  services.authelia.instances.${autheliaInstance} = {
+    enable = true;
+    settings = {
+      theme = "auto";
+      server.address = "tcp://127.0.0.1:${toString autheliaPort}/";
+      log.level = "info";
+
+      authentication_backend.file.path = config.sops.secrets."authelia/users".path;
+
+      access_control = {
+        default_policy = "deny";
+        rules = [
+          {
+            domain = "*.${domain}";
+            policy = "one_factor";
+          }
+        ];
+      };
+
+      storage.local.path = "/var/lib/authelia-${autheliaInstance}/db.sqlite3";
+
+      session.cookies = [
+        {
+          inherit domain;
+          authelia_url = "https://auth.${domain}";
+          inactivity = "1M";
+          expiration = "3M";
+          remember_me = "1y";
+        }
+      ];
+
+      notifier.filesystem.filename = "/var/lib/authelia-${autheliaInstance}/notifications.txt";
+
+      # Necessary for Caddy integration
+      # See https://www.authelia.com/integration/proxies/caddy/#implementation
+      server.endpoints.authz.forward-auth.implementation = "ForwardAuth";
+    };
+    secrets = {
+      jwtSecretFile = config.sops.secrets."authelia/jwt_secret".path;
+      sessionSecretFile = config.sops.secrets."authelia/session_secret".path;
+      storageEncryptionKeyFile = config.sops.secrets."authelia/storage_encryption_key".path;
+    };
+  };
+
+  # Caddy - reverse proxy
+  services.caddy = {
+    enable = true;
+    virtualHosts."auth.${domain}".extraConfig = ''
+      reverse_proxy localhost:${toString autheliaPort}
+    '';
+    # Snippet for protecting other services with Authelia
+    # Import with: import auth
+    extraConfig = ''
+      (auth) {
+        forward_auth localhost:${toString autheliaPort} {
+          uri /api/authz/forward-auth
+          copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+        }
+      }
+    '';
+  };
 
   # Passwordless sudo via SSH agent forwarding
   security.pam.rssh.enable = true;
