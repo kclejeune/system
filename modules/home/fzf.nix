d05113403c8b51512201f9142a8db341f111993c
Kennan LeJeune <kennan@case.edu>
2026-07-07 10:22:55 -0400

    fix fzf module eval warnings

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -1,6 +1,6 @@
 _: {
   flake.homeModules.fzf =
-    { pkgs, lib, ... }:
+    { config, pkgs, lib, ... }:
     let
       fd = lib.getExe pkgs.fd;
     in
@@ -9,14 +9,15 @@ _: {
         enable = true;
         defaultCommand = "${fd} -H --type f";
         defaultOptions = [ "--height 50%" ];
-        fileWidgetCommand = "${defaultCommand}";
-        fileWidgetOptions = [
+        fileWidget.command = "${defaultCommand}";
+        fileWidget.options = [
           "--preview '${lib.getExe pkgs.bat} --color=always --plain --line-range=:200 {}'"
         ];
         tmux.enableShellIntegration = true;
-        changeDirWidgetCommand = "${fd} -H --type d";
-        changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
-        historyWidgetOptions = [ ];
+        changeDirWidget.command = "${fd} -H --type d";
+        changeDirWidget.options = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+        # atuin owns Ctrl-R; disable fzf's history widget binding when it's enabled.
+        historyWidget.command = lib.mkIf config.programs.atuin.enable "";
       };
     };
 }
