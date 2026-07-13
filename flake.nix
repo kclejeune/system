53a430f44e107339d35c2fa76ec31557d396da55
Kennan LeJeune <kennan@case.edu>
2026-07-13 13:43:08 -0400

    bump nimbus, drop proxied substituters, add remaining public keys

--- flake.nix
+++ flake.nix
@@ -4,12 +4,11 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.kclj.io"
-      "https://install.determinate.systems"
-      "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.kclj.io-1:StGAmbogIZLS5IAQD2IQCbbmIjv3Sq8rl/AVEw4Sy7s="
       "kclejeune:u0sa4anVXC4bKlzEsijdSlLyWVaEkApu6KWyDbbJMkk="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
